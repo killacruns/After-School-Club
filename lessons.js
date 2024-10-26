a335b9bff46b8a3c8811eae53a57517629bbb050
killacruns <mugoasman@gmail.com>
2024-10-26 14:44:24 +0400

    additional lessons

---
+++ lessons.js
@@ -0,0 +1,107 @@
+let lessons =  [
+    {
+      id: 1,
+      subject: "Badminton",
+      location: "Koln",
+      price: 150,
+      spaces: 5,
+      images: "images/badminton.jpg",
+    },
+    {
+      id: 2,
+      subject: "Boardgames",
+      location: "Manchester",
+      price: 145,
+      spaces: 5,
+      images: "images/boardgames.jpg",
+    },
+    {
+      id: 3,
+      subject: "Chess",
+      location: "British Colombia",
+      price: 180,
+      spaces: 5,
+      images: "images/chess.jpg",
+    },
+    {
+      id: 4,
+      subject: "Choir",
+      location: "Bordeaux",
+      price: 200,
+      spaces: 5,
+      images: "images/choir.jpg",
+    },
+    {
+      id: 5,
+      subject: "English",
+      location: "London",
+      price: 350,
+      spaces: 5,
+      images: "images/english.jpg",
+    },
+    {
+      id: 6,
+      subject: "Film",
+      location: "New York",
+      price: 325,
+      spaces: 5,
+      images: "images/film.jpg",
+    },
+    {
+      id: 7,
+      subject: "French",
+      location: "Lyon",
+      price: 80,
+      spaces: 5,
+      images: "images/french.jpg",
+    },
+    {
+      id: 8,
+      subject: "Gymnastics",
+      location: "Hong Kong",
+      price: 550,
+      spaces: 5,
+      images: "images/gymnastics.jpg",
+    },
+    {
+      id: 9,
+      subject: "Mathematics",
+      location: "Nairobi",
+      price: 245,
+      spaces: 5,
+      images: "images/mathematics.jpg",
+    },
+    {
+      id: 10,
+      subject: "Swimming",
+      location: "Kilifi",
+      price: 600,
+      spaces: 5,
+      images: "images/swimming.jpg",
+    },
+  ];
+
+  //data to keep track of information;
+  let order = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    state: "",
+  };
+  
+  //data to keep track of the states.
+  let states = {
+    KLN: "Koln",
+    MAN: "Manchester",
+    BC: "British Colombia",
+    BOR: "Bordeaux",
+    LON: "London",
+    NY: "New York",
+    LY: "Lyon",
+    HK: "Hong Kong",
+    NAI: "Nairobi",
+    KI: "Kilifi",
+  };
+
+  
